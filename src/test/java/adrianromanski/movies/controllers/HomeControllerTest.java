9b0d2d2202148b9ebdb40247bc1fb946c51d29e1
Adrian Romański <muszynianka1337@gmail.com>
2020-05-19 18:02:58 +0200

    Added CircleCi and CodeCoverage

---
+++ src/test/java/adrianromanski/movies/controllers/HomeControllerTest.java
@@ -0,0 +1,37 @@
+package adrianromanski.movies.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+class HomeControllerTest {
+
+    HomeController homeController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        homeController = new HomeController();
+
+        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+    }
+
+    @Test
+    void home() throws Exception {
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+}
