d72153112b84e6e2f110bc0f6c22b7e259c23c49
Adrian Romański <muszynianka1337@gmail.com>
2020-05-21 14:37:25 +0200

    Testing CategoryService

---
+++ src/test/java/adrianromanski/movies/services/CategoryServiceImplTest.java
@@ -0,0 +1,47 @@
+package adrianromanski.movies.services;
+
+import adrianromanski.movies.domain.Category;
+import adrianromanski.movies.mapper.CategoryMapper;
+import adrianromanski.movies.mapper.CategoryMapperImpl;
+import adrianromanski.movies.model.CategoryDTO;
+import adrianromanski.movies.repositories.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class CategoryServiceImplTest {
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    CategoryService categoryService;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        CategoryMapper categoryMapper = new CategoryMapperImpl();
+        categoryService = new CategoryServiceImpl(categoryRepository, categoryMapper);
+    }
+
+    @DisplayName("Happy Path, method = getAllCategories")
+    @Test
+    void getAllCategories() {
+        List<Category> categories = Arrays.asList(new Category(), new Category(), new Category());
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        List<CategoryDTO> returnDTO = categoryService.getAllCategories();
+
+        assertEquals(returnDTO.size(), 3);
+    }
+}
