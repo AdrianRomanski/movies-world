7c7c8085a583cade881490b5223449ffea18a938
Adrian Romański <muszynianka1337@gmail.com>
2020-05-23 11:37:31 +0200

    Testing Category Service and Controller

---
+++ src/test/java/adrianromanski/movies/controllers/CategoryControllerTest.java
@@ -0,0 +1,55 @@
+package adrianromanski.movies.controllers;
+
+import adrianromanski.movies.domain.Category;
+import adrianromanski.movies.domain.Movie;
+import adrianromanski.movies.model.CategoryDTO;
+import adrianromanski.movies.model.MovieDTO;
+import adrianromanski.movies.services.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+class CategoryControllerTest {
+
+    @Mock
+    CategoryService categoryService;
+
+    @InjectMocks
+    CategoryController categoryController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(categoryController).build();
+    }
+
+    @DisplayName("GET, HappyPath, method = getAllMoviesForCategory")
+    @Test
+    void getAllMoviesForCategory() throws Exception {
+        List<MovieDTO> movies = Arrays.asList( new MovieDTO(),  new MovieDTO(), new MovieDTO());
+
+        when(categoryService.getAllMoviesForCategory(anyString())).thenReturn(movies);
+
+        mockMvc.perform(get("/category/fantasy"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("movies"));
+    }
+}
--- src/test/java/adrianromanski/movies/services/CategoryServiceImplTest.java
+++ src/test/java/adrianromanski/movies/services/CategoryServiceImplTest.java
@@ -1,11 +1,14 @@
 package adrianromanski.movies.services;
 
 import adrianromanski.movies.domain.Category;
+import adrianromanski.movies.domain.Movie;
+import adrianromanski.movies.exceptions.ResourceNotFoundException;
 import adrianromanski.movies.mapper.CategoryMapper;
 import adrianromanski.movies.mapper.CategoryMapperImpl;
 import adrianromanski.movies.mapper.MovieMapper;
 import adrianromanski.movies.mapper.MovieMapperImpl;
 import adrianromanski.movies.model.CategoryDTO;
+import adrianromanski.movies.model.MovieDTO;
 import adrianromanski.movies.repositories.CategoryRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,8 +18,12 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 class CategoryServiceImplTest {
@@ -46,5 +53,26 @@ class CategoryServiceImplTest {
         List<CategoryDTO> returnDTO = categoryService.getAllCategories();
 
         assertEquals(returnDTO.size(), 3);
+    }
+
+    @DisplayName("Happy Path, method = getAllMoviesForCategory")
+    @Test
+    void getAllMoviesForCategoryHappyPath() {
+        List<Movie> movies = Arrays.asList(new Movie(), new Movie(), new Movie());
+        Category category = Category.builder().movies(movies).build();
+
+        when(categoryRepository.findByName(anyString())).thenReturn(Optional.of(category));
+
+        List<MovieDTO> returnDTO = categoryService.getAllMoviesForCategory(anyString());
+
+        assertEquals(returnDTO.size(), 3);
+    }
+
+    @DisplayName("UnhappyPath, method = getAllMoviesForCategory")
+    @Test
+    void getAllMovieForCategoryUnHappyPath() {
+        Throwable ex = catchThrowable(() -> categoryService.getAllMoviesForCategory("24421"));
+
+        assertThat(ex).isInstanceOf(ResourceNotFoundException.class);
     }
 }
