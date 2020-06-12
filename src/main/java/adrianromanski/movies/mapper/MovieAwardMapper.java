e3c76c7e47a228f93a19dfee7b8f1a06f7db5d7c
Adrian Romański <muszynianka1337@gmail.com>
2020-06-12 13:58:23 +0200

    Added MovieAwardMapper and Test

---
+++ src/main/java/adrianromanski/movies/mapper/MovieAwardMapper.java
@@ -0,0 +1,16 @@
+package adrianromanski.movies.mapper;
+
+import adrianromanski.movies.domain.award.MovieAward;
+import adrianromanski.movies.model.award.MovieAwardDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel="spring")
+public interface MovieAwardMapper {
+
+    @Mapping(source = "movie", target = "movieDTO")
+    MovieAwardDTO awardToAwardDTO(MovieAward award);
+
+    @Mapping( source = "movieDTO", target = "movie")
+    MovieAward awardDTOToAward(MovieAwardDTO awardDTO);
+}
