463e3724b332064bec07b96de2b579ce16d92ae5
adrianromanski <adrianromanski@icloud.com>
2020-09-20 11:31:17 +0200

    #8 Added User NavBar Fragment, replace code for Movies

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,5 +1,4 @@
-
-    $(document).ready(function(){
+$(document).ready(function(){
     $('.dropdown-submenu a.test').on("click", function(e){
         $(this).next('ul').toggle();
         e.stopPropagation();
