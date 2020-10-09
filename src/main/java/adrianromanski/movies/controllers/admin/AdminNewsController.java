ffbb7b00e081ce6d604ff2d5f81a76f91065c4ab
adrianromanski <adrianromanski@icloud.com>
2020-10-09 10:47:17 +0200

    Working on #1. Show News

---
+++ src/main/java/adrianromanski/movies/controllers/admin/AdminNewsController.java
@@ -0,0 +1,38 @@
+package adrianromanski.movies.controllers.admin;
+
+import adrianromanski.movies.domain.base_entity.News;
+import adrianromanski.movies.services.news.NewsService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@AllArgsConstructor
+public class AdminNewsController {
+
+    private final NewsService newsService;
+
+
+    @GetMapping("/admin/news/showNews/page/{page}")
+    public ModelAndView getNewsPaged(@PathVariable int page) {
+        ModelAndView modelAndView = new ModelAndView("admin/news/showNewsForm");
+        PageRequest pageable = PageRequest.of(page - 1, 12);
+        Page<News> newPage = newsService.getNewsPaged(pageable);
+        int totalPages = newPage.getTotalPages();
+        if(totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1,totalPages).boxed().collect(Collectors.toList());
+            modelAndView.addObject("pageNumbers", pageNumbers);
+        }
+        modelAndView.addObject("activeNewsList", true);
+        modelAndView.addObject("newsList", newPage.getContent());
+        return modelAndView;
+    }
+}
---
+++ src/test/java/adrianromanski/movies/controllers/admin/AdminNewsControllerTest.java
@@ -0,0 +1,58 @@
+package adrianromanski.movies.controllers.admin;
+
+import adrianromanski.movies.domain.base_entity.News;
+import adrianromanski.movies.services.news.NewsServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class AdminNewsControllerTest {
+
+    @InjectMocks
+    AdminNewsController controller;
+
+    @Mock
+    NewsServiceImpl newsService;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    @DisplayName("GET, method = getNewsPaged")
+    void getNewsPaged() throws Exception {
+        // Given
+        List<News> movieList = Arrays.asList(new News(), new News(), new News());
+        PageRequest pageable = PageRequest.of(0, 12);
+        Page<News> newsPage = new PageImpl<>(movieList, pageable, movieList.size());
+
+        //When
+        when(newsService.getNewsPaged(pageable)).thenReturn(newsPage);
+
+        //Then
+        mockMvc.perform(get("/admin/news/showNews/page/1"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("newsList"))
+                .andExpect(view().name("admin/news/showNewsForm"));
+    }
+}
